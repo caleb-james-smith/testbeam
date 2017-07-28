ebca4183fe7111d8ad62f0ac43531819c4dd9406
Caleb James Smith <caleb_smith2@baylor.edu>
2017-07-28 10:48:48 +0200

    simplify gif creation using glob

--- makeGif.py
+++ makeGif.py
@@ -1,22 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import imageio
+import glob
 
 def makeGif(name):
-  directory = name
-  name = name + '/' + name
-  filenames = name + '.txt'
-  gifname   = name + '.gif'
-  print filenames
-  with open(filenames,'r') as fn:
-    #with imageio.get_writer(gifname, mode='I',duration=0.01) as writer:
-    with imageio.get_writer(gifname, mode='I', fps=1000) as writer:
-      for filename in fn:
-        filename = filename.split('\n')[0]
-        filename = directory + '/' + filename
-        print filename
-        image = imageio.imread(filename)
-        writer.append_data(image)
+  gifname = name + '.gif'
+  files = glob.glob(name + '/*.gif')
+  #use duration or fps for timing
+  #with imageio.get_writer(gifname, mode='I',duration=0.01) as writer:
+  with imageio.get_writer(gifname, mode='I', duration=1) as writer:
+    for filename in files:
+      print filename
+      image = imageio.imread(filename)
+      writer.append_data(image)
+  print "Created gif: {0}".format(gifname)
 
 if __name__ == "__main__":
   makeGif("eta_scan_phi_5")

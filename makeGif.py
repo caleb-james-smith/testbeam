ce065d2f392b99e324e575d2e8563629ea37f4f0
Caleb James Smith <caleb_smith2@baylor.edu>
2017-07-28 10:33:45 +0200

    update with new plots

--- makeGif.py
+++ makeGif.py
@@ -3,12 +3,17 @@ import numpy as np
 import imageio
 
 def makeGif(name):
+  directory = name
+  name = name + '/' + name
   filenames = name + '.txt'
-  gifname = name + '.gif'
+  gifname   = name + '.gif'
+  print filenames
   with open(filenames,'r') as fn:
-    with imageio.get_writer(gifname, mode='I',duration=1) as writer:
+    #with imageio.get_writer(gifname, mode='I',duration=0.01) as writer:
+    with imageio.get_writer(gifname, mode='I', fps=1000) as writer:
       for filename in fn:
         filename = filename.split('\n')[0]
+        filename = directory + '/' + filename
         print filename
         image = imageio.imread(filename)
         writer.append_data(image)

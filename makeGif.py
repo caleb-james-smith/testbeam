5759c87be469908f9075216b7458aeec5705abeb
Caleb James Smith <caleb_smith2@baylor.edu>
2017-07-30 00:23:07 +0200

    updates and phi 6 scan

--- makeGif.py
+++ makeGif.py
@@ -7,8 +7,7 @@ def makeGif(name):
   gifname = name + '.gif'
   files = glob.glob(name + '/*.gif')
   #use duration or fps for timing
-  #with imageio.get_writer(gifname, mode='I',duration=0.01) as writer:
-  with imageio.get_writer(gifname, mode='I', duration=1) as writer:
+  with imageio.get_writer(gifname, mode='I', duration=0.2) as writer:
     for filename in files:
       print filename
       image = imageio.imread(filename)
@@ -16,4 +15,5 @@ def makeGif(name):
   print "Created gif: {0}".format(gifname)
 
 if __name__ == "__main__":
-  makeGif("eta_scan_phi_5")
+  makeGif("run_2921_eta_scan_phi_5")
+  makeGif("run_2921_eta_scan_phi_6")
